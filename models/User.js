408687b5501ae43dbb4235b3eb2b4383481931e2
amarten <alexdmarten@gmail.com>
2022-03-10 19:46:13 -0800

    inital project structure and User schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9\.-_]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address!']
+        },
+        thought: [],
+        friends: []
+    }
+);
+
+// UserSchema.virtual('friendCount').get(function() {
+//     return this.friends
+// })
+
+const User = model('User', UserSchema);
+
+module.exports = User;
